8e184effa99d164a7150a0a5c07113317bf6f8a2
Justin Reid <justin_reid@sbcglobal.net>
2019-03-16 10:18:24 -0700

    Update SqlQueries.sql

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -14,3 +14,23 @@ WHERE publisherName = 'publisherName';
 
 -- List all book titles
 SELECT bookTitle FROM Books;
+
+-- List all the data for a single book specified by the user
+SELECT * FROM Books
+WHERE bookTitle = 'bookTitle';
+
+-- Insert a new book
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages)
+Value('groupName', 'bookTitle', 'publisherName', 'yearPublished', 'numberPages');
+
+-- Insert a new publisher and update all book published by one publisher to be published by the new pubisher.
+INSERT INTO Publisher (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail)
+Value('publisherName', 'publicAddress', 'publisherAddress', 'publisherPhone', 'publisherEmail');
+UPDATE Books SET publisherName = 'newPublisher'
+WHERE publisherName = 'oldPublisher';
+DELETE FROM Publishers
+WHERE publisherName = 'oldPublisher';
+
+-- Remove a single book specified by the user
+DELETE FROM Books
+WHERE bookTitle = 'bookTitle'; 
