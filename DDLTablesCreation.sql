a88d65f8aa0b8815dbe012d19b963eb1ca74719f
KermiteO <kermicheo@gmail.com>
2019-03-16 09:26:15 -0700

    Add files via upload

---
+++ DDLTablesCreation.sql
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <iomanip>
+#include <string>
+#include <limits>
+#include <ios>
+using namespace std;
+
+BigInt GoldRabbit(int n);
+
+ostream& operator<<(ostream& os,const BigInt& dt);
+
+int fact(int n);
